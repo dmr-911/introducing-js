91a665f16863866274497ccfa456fcbd2750dc6c
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-30 22:46:56 +0600

    numeric,string and boolean variable exampless

--- variable.js
+++ variable.js
@@ -1,4 +1,14 @@
+// Numeric variables
 var ricePrice = 30;
 var temperature = 37;
 var age = 25;
-console.log(ricePrice,temperature,age);
+
+// String variables
+var name = 'Dewan Mizanur Rahman';
+var address = 'vuter goli';
+var firstLove = 'MR';
+
+// Boolean variables
+var pass = true;
+var subscribe = false;
+console.log(ricePrice,temperature,age,address,firstLove,pass,subscribe);
