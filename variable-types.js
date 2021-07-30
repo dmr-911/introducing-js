1cb95330d77d4961aad7dc6979eb2d729f5a3123
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-31 00:04:07 +0600

    variable types

---
+++ variable-types.js
@@ -0,0 +1,20 @@
+var tShirtPrice = 300;
+var quantity = '4';
+var raining = false;
+var romantic;
+console.log(typeof romantic);
+console.log(typeof tShirtPrice);
+console.log(typeof raining);
+console.log(typeof quantity);
+
+var today = new Date();
+console.log(today);
+
+// special edition
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total = total.toFixed(1);
+total = parseFloat(total)
+console.log(total);
+// console.log(parseFloat(total.toFixed(1)));
