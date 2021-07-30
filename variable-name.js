3e2674d0c54fc7a7382f4d61e96a7c8f319ef737
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-30 23:00:22 +0600

    best practice of variable names

---
+++ variable-name.js
@@ -0,0 +1,10 @@
+var name = 'Hero Alam';
+
+// Reserve keyword not allowed
+// Name should be one word 
+// quote is not allowed
+// first letter can not be uppercase
+var my-office-address = 'Dhaka 1200'; //not valid
+var my_office_address = 'Dhaka 1200';
+var myOfficeAddress = 'Dhaka 1200'; //camel case(Best practice for javaScript)
+var myofficeaddress = 'Dhaka 1200';
