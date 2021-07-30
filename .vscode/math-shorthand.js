4320d80a749b26ec84f3384ae6102090ffb97bcf
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-30 23:30:37 +0600

    math shorthand added

---
+++ .vscode/math-shorthand.js
@@ -0,0 +1,20 @@
+// Recap 
+var price1 = 31;
+var price2 = 10;
+var sum = price1+price2;
+var difference = price2 - price1;
+var multiply = price1 * price2;
+var division = price1 / price2;
+
+var doublePrice = price2 * 2;
+var newPrice = price1+10;
+price1 = price1 + 20;
+price1 += 20;
+console.log(newPrice);
+console.log(price1);
+
+var age = 24;
+// age = age + 1;
+// age += 1; //shorter version 
+age++; //for adding only 1
+console.log(age);
