a9cc08a9ceab98baef23dfb0753e3c3519ff7b69
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-30 23:48:24 +0600

    conversion added

---
+++ conversion.js
@@ -0,0 +1,16 @@
+var firstName = 'Dewan Mizanur';
+var lastName = 'Rahman';
+var fullName= firstName +' '+ lastName;
+console.log(fullName);
+
+// integer
+var sunglass = 3;
+// float
+var price = 99.99;
+
+var onionPrice = '42';
+var eggPrice = '30.35';
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+var total = onionPriceNumber + eggPriceNumber;
+console.log(total);
