2619eb51b7f93e2b968a4d1d15770f380b6af52e
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-07-30 23:15:28 +0600

    add,substract,multiply,division

---
+++ mathmatical-operation.js
@@ -0,0 +1,18 @@
+var onionPrice = 43;
+var eggPrice = 30;
+var eggQuantity = 4;
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice+eggPrice);
+
+var total = onionPrice+eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var eggPriceMultiply = eggPrice * eggQuantity;
+console.log(total);
+console.log(priceDifference);
+console.log(eggPriceMultiply);
+
+var availableMoney = 100;
+var orangePrice = 5;
+var singleQuantity = availableMoney/orangePrice;
+console.log(singleQuantity);
--- variable-name.js
+++ variable-name.js
@@ -4,7 +4,7 @@ var name = 'Hero Alam';
 // Name should be one word 
 // quote is not allowed
 // first letter can not be uppercase
-var my-office-address = 'Dhaka 1200'; //not valid
+// var my-office-address = 'Dhaka 1200'; //not valid
 var my_office_address = 'Dhaka 1200';
 var myOfficeAddress = 'Dhaka 1200'; //camel case(Best practice for javaScript)
 var myofficeaddress = 'Dhaka 1200';
